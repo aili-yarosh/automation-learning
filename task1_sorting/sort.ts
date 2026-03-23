e0a2a32235a6d2dfc9ad651cc118c705ce7479ae
Оля Ярош <olya.yarosshh@gmail.com>
2026-03-23 14:42:38 +0200

    feat(task1-sorting): parse JSON data and log object
    Removed debug log of data type and added JSON parsing to transform
    string data into object for further processing.

--- task1_sorting/sort.ts
+++ task1_sorting/sort.ts
@@ -1,3 +1,5 @@
 const fs = require('fs'); // працюємо з файловою системою тому підключаємо модуль файлової системи
 const data = fs.readFileSync('./data_for_sort.json',{encoding : 'utf8'}); //читаємо json файл та додали декодування
-console.log(typeof data);
+//console.log(typeof data);
+let dataObj = JSON.parse(data); //стрінг трансформуємо в обʼєкт ts
+console.log(dataObj);
