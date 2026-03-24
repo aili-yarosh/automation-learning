da6f60e4a9a68696afbf1a7fd99985b8d51332a8
Оля Ярош <olya.yarosshh@gmail.com>
2026-03-24 19:34:17 +0200

    The change replaces logging of the entire parsed data object with logging the name property of the first object to verify successful parsing and access object properties.

--- task1_sorting/sort.ts
+++ task1_sorting/sort.ts
@@ -2,4 +2,5 @@ const fs = require('fs'); // працюємо з файловою системо
 const data = fs.readFileSync('./data_for_sort.json',{encoding : 'utf8'}); //читаємо json файл та додали декодування
 //console.log(typeof data);
 let dataObj = JSON.parse(data); //стрінг трансформуємо в обʼєкт ts
-console.log(dataObj); //перевіряємо чи дійсно обʼєкт через typeof
+console.log(dataObj); //перевіряємо через typeof чи дійсно обʼєкт
+console.log(dataObj[0].name); // виводимо змінну 0-го елементу в масиві
