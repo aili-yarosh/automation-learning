250602db1a3dca43ace929a77bb04bf9940a756d
Оля Ярош <olya.yarosshh@gmail.com>
2026-03-25 21:08:40 +0200

    refactor(task1-sorting): update logging and assign parsed data to Fruit constant Assign parsed data to const Fruit and log first fruit name for clarity.

--- task1_sorting/sort.ts
+++ task1_sorting/sort.ts
@@ -1,6 +1,9 @@
 const fs = require('fs'); // працюємо з файловою системою тому підключаємо модуль файлової системи
 const data = fs.readFileSync('./data_for_sort.json',{encoding : 'utf8'}); //читаємо json файл та додали декодування
 //console.log(typeof data);
-let dataObj = JSON.parse(data); //стрінг трансформуємо в обʼєкт ts
-console.log(dataObj); //перевіряємо через typeof чи дійсно обʼєкт
-console.log(dataObj[0].name); // виводимо змінну 0-го елементу в масиві
+let dataObj = JSON.parse(data) ; //стрінг трансформуємо в обʼєкт ts
+//console.log(dataObj); //перевіряємо через typeof чи дійсно обʼєкт
+//console.log(dataObj[2].name); // виводимо змінну 2-го елементу в масиві
+
+const Fruit = dataObj;
+console.log(Fruit[0].name); 
