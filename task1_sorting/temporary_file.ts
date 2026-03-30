0525b4cb4f7392afa6e2695614bd6440b7db400d
Оля Ярош <olya.yarosshh@gmail.com>
2026-03-30 19:00:38 +0300

    chore(task1-sorting): add temporary file with cat data logging

---
+++ task1_sorting/temporary_file.ts
@@ -0,0 +1,8 @@
+const fs = require('fs');
+let name;
+name = 'cat';
+let skills;
+skills = 'lapki';
+let isQa = false;
+
+console.log('I am a'+' '+name+' '+'and I have'+' '+skills + '. I am QA:'+''+isQa );
